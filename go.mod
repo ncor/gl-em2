de4e0902995e09596c0ca698b4673d8c1ea61310
ncor <toffuyume@gmail.com>
2024-10-24 16:07:47 +0300

    feat,chore: update deps, add error handling

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ncor/gl-em2
 
 go 1.23.2
 
-require github.com/ncor/gl-em1 v0.0.1
+require github.com/ncor/gl-em1 v0.0.2
--- hello.go
+++ hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	greetings "github.com/ncor/gl-em1"
 )
 
 func main() {
-	message := greetings.Hello("ensi")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("ensi")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
